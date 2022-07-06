876e5a94f4f57d2e16919a0c4612803491fbfae1
kaywish <kaywishawaan@gmail.com>
2022-07-06 19:08:09 -0400

    added movement to character

--- app/app.js
+++ app/app.js
@@ -1,12 +1,30 @@
+const showStatus= () => {
+    alert("welcome to the game")
+    }
+
+
+
 class Tamagotchi{
     constructor(name){
     this.name= "Tamagotchi"
     this.hunger= 0
     this.sleepiness=0
     this.boredom=0
-    this.age=""
+    this.age=0
+
+    
+}
+
+
+
+
+
+
 
 }
-}
+
+
+
+
 
 
