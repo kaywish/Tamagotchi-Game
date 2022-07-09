104293983adbcf98fc5248fce5da25a0ee96609a
kaywish <kaywishawaan@gmail.com>
2022-07-09 12:29:28 -0400

    its a long road

--- app/app.js
+++ app/app.js
@@ -1,8 +1,9 @@
 // NAME YOUR PET //
-// const nameMe = prompt(" Name your pet")
-// const whatUp= document.querySelector("#name")
+alert(" Welcome to Tamagotchi")
+const nameMe = prompt(" Name your pet")
+const whatUp= document.querySelector("#name")
 
-// whatUp.innerText=`Name : ${nameMe}`
+whatUp.innerText=`Name : ${nameMe}`
 
 
 
@@ -20,22 +21,92 @@ class Tamagotchi{
 
 
  newGame(){
-// console.log("Hunger level is "+this.hunger)
-// console.log("Sleepiness level is"+ this.sleepiness)
-// console.log("Boredom level is " +this.boredom) 
-// console.log("Age is " + this.age)
+console.log("Hunger level is "+this.hunger)
+console.log("Sleepiness level is"+ this.sleepiness)
+console.log("Boredom level is " +this.boredom) 
+console.log("Age is " + this.age)
+//////////////////
+//Food
 const passTime = setInterval(() => {
     const eatNow = document.getElementById("food")
     eatNow.innerHTML=("Food Level: " + this.hunger)
     this.hunger++
  
-if (this.hunger == 4) {
+if (this.hunger == 10) {
     alert("Your pet died")
 
 }
 
 console.log(passTime)
 }, 3000);
+
+/////////////////////////////
+// Sleep
+const sleepTime = setInterval(() => {
+    const eatNow = document.getElementById("energy")
+    eatNow.innerHTML=("Energy Level: " + this.sleepiness)
+    this.sleepiness++
+ 
+if (this.sleepiness == 10) {
+    alert("Your pet died")
+
+}
+
+console.log(sleepTime)
+}, 3000);
+
+/////////////////////////////
+// Fun
+
+const funTime = setInterval(() => {
+    const eatNow = document.getElementById("fun")
+    eatNow.innerHTML=("Fun Level: " + this.boredom)
+    this.boredom++
+ 
+if (this.boredom == 10) {
+    alert("Your pet died")
+
+}
+
+console.log(funTime)
+}, 3000);
+
+///////////////////////////////
+// Age
+
+const ageTime = setInterval(() => {
+    const eatNow = document.getElementById("age")
+    eatNow.innerHTML=("Age: " + this.age)
+    this.age++
+ 
+if (this.age == 3) {
+    alert("Your pet grew!")
+
+}
+
+if (this.age == 6){
+    alert(" Your pet grew!")
+}
+
+if (this.age == 9){
+    alert(" Your pet wont stop growing!")
+}
+
+
+
+
+
+console.log(ageTime)
+}, 6000);
+
+
+
+
+
+
+
+
+
 
 }
 
@@ -53,16 +124,17 @@ const passTime = setInterval(() => {
         
     }
  
-// if (this.hunger == 4) {
-//     alert("Your pet died")
-
-// }
-
 console.log(passTime)
 }, 2000);
 
 console.log("Your pet ate an apple ")
- }
+}
+
+
+
+
+
+
 
  ///////////
 
@@ -87,20 +159,8 @@ lightOn() {
 
 
 
-countUp() {
-if (this.boredom == 10) 
-{console.log(" Your pet died")
-    }
-    if (this.hunger == 10) {
-        console.log(" Your pet died")
-    }
-    if (this.sleepiness == 10) {
-        console.log(" Your pet died")
-    }
-   
 
 
-}
 test() {
     alert("work")
 }
@@ -138,10 +198,26 @@ document.getElementById("rest").addEventListener("click", (e) => rest() )
 document.getElementById("play").addEventListener("click", (e) => playNow() )    
 
 // Light Switch
-const getOn =document.getElementById("light on")
+// const backGround= ("#000000")
+const getOn =document.getElementById("light off")
 getOn.addEventListener("click", (e) => {
-  
-   player.lightOn()})
+    console.log("hi",document.querySelector(".background").src)
+    
+    document.querySelector(".background").src="Night.jpg"
+})
+
+const getOff= document.getElementById("light on")
+getOff.addEventListener("click", (e) => {
+
+    console.log("hi",document.querySelector(".background").src)
+
+    document.querySelector(".background").src="Background.jpg"
+})
+
+
+
+
+   player.lightOn()
   
 
 
